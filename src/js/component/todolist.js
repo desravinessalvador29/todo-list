f95d3293af0e74cd342ca105f1f7e00f7b5a4779
Salvador Desravines <desravines.salvador001@yahoo.com>
2019-08-01 22:52:04 +0000

    starting

---
+++ src/js/component/todolist.js
@@ -0,0 +1,12 @@
+import React from "react";
+export class Todolist extends React.Component {
+	render() {
+		return (
+			<React.Fragment>
+				<div className="container">
+					<h1>todos</h1>
+				</div>
+			</React.Fragment>
+		);
+	}
+}
