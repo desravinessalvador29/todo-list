390e8561f4f16280790976e821254cfc0b79924c
Salvador Desravines <desravines.salvador001@yahoo.com>
2019-08-03 15:47:00 +0000

    added input

--- src/js/component/todolist.js
+++ src/js/component/todolist.js
@@ -1,10 +1,43 @@
 import React from "react";
 export class Todolist extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			list: []
+		};
+		this.onAddItem = this.onAddItem.bind(this);
+	}
+
+	onAddItem = e => {
+		let newItem = e.target.value;
+		this.setState({
+			list: this.state.list.concat(newItem)
+		});
+	};
+
 	render() {
 		return (
 			<React.Fragment>
 				<div className="container">
-					<h1>todos</h1>
+					<h1>To-do List</h1>
+					<div className="items">
+						<div>
+							<input
+								placeholder="Add Items"
+								type="text"
+								onKeyPress={e =>
+									e.charCode === 13 ? this.onAddItem(e) : null
+								}
+							/>
+						</div>
+						<ul className="list-group">
+							{this.state.list.map((value, index) => (
+								<li key={index} className="list-group-item">
+									{value}
+								</li>
+							))}
+						</ul>
+					</div>
 				</div>
 			</React.Fragment>
 		);
