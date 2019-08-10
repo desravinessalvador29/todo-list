83b618cd58e5cb020c17acf761a572ae2b48f29c
Salvador Desravines <desravines.salvador001@yahoo.com>
2019-08-10 00:20:51 +0000

    added put

--- src/js/component/todolist.js
+++ src/js/component/todolist.js
@@ -6,15 +6,44 @@ export class Todolist extends React.Component {
 			list: []
 		};
 	}
+
+	componentDidMount() {
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/saldesravines29"
+		)
+			.then(resp => resp.json())
+			.then(data => {
+				//console.log(data);
+				this.setState({ list: data });
+			})
+			.catch(error => {
+				//console.log(error);
+			});
+	}
 	render() {
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/saldesravines29",
+			{
+				method: "PUT",
+				body: JSON.stringify(data),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			}
+		)
+			.then(res => res.json())
+			.then(response => JSON.stringify(response))
+			.catch(error => error);
+
 		return (
 			<React.Fragment>
 				<div className="container">
 					<h1>To-do List</h1>
 					<div className="items">
 						<div>
+							<span>Username</span>
 							<input
-								placeholder="Add Items"
+								placeholder="Add Username"
 								type="text"
 								onKeyPress={e =>
 									e.charCode === 13 &&
@@ -25,23 +54,39 @@ export class Todolist extends React.Component {
 									})
 								}
 							/>
+							<div className="task">
+								<span>Task</span>
+								<input
+									placeholder="Add Items"
+									type="text"
+									onKeyPress={e =>
+										e.charCode === 13 &&
+										this.setState({
+											list: this.state.list.concat(
+												e.target.value
+											)
+										})
+									}
+								/>
+							</div>
 						</div>
 						<ul className="list-group">
-							{this.state.list.map((value, index) => (
-								<li key={index} className="list-group-item">
-									{value}{" "}
-									<button
-										onClick={e =>
-											this.setState({
-												list: this.state.list.filter(
-													(item, i) => i !== index
-												)
-											})
-										}>
-										delete
-									</button>
-								</li>
-							))}
+							{this.state &&
+								this.state.list.map((value, index) => (
+									<li key={index} className="list-group-item">
+										{value.label}{" "}
+										<button
+											onClick={e =>
+												this.setState({
+													list: this.state.list.filter(
+														(item, i) => i !== index
+													)
+												})
+											}>
+											delete this item
+										</button>
+									</li>
+								))}
 						</ul>
 					</div>
 				</div>
